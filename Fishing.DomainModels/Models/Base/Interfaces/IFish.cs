c9bf4b27da3234a780b53484a0b2ad676e057743
DeltaCreeper <spagrave@gmail.com>
2019-04-16 21:56:48 +0200

    Added initial project structure.

---
+++ Fishing.DomainModels/Models/Base/Interfaces/IFish.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fishing.DomainModels.Models.Base.Interfaces
+{
+    public interface IFish
+    {
+        void Swim();
+    }
+}
